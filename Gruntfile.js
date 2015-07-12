70bbd9d9de652dcf0d46f81d9ab2d8dfb7f8771a
Michael Bowen <mikee.bowen@gmail.com>
2015-07-12 09:51:54 -0700

    add linting and move methods into method folder

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+ module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-jscs');
+
+  //initialize grunt
+  grunt.initConfig({
+    //create jshint task
+    jshint: {
+      dev: {
+        //tell jshint what to check
+        src: ['Gruntfile.js', 'methods/**/*.js'],
+        options: {
+          node: true,
+          globals: {
+            describe: true,
+            it: true,
+            before: true,
+            after: true,
+            beforeEach: true,
+            afterEach: true,
+            res: true
+          }
+        }
+      },
+      //tell mocha where test files are
+      mocha: {
+        src: ['test/**/*.js'],
+        options: {
+          node: true,
+          globals: {
+            describe: true,
+            it: true,
+            before: true,
+            after: true,
+            beforeEach: true,
+            afterEach: true,
+            res: true,
+            expect: true
+          }
+        }
+      },
+      // create jscs task
+      jscs: {
+        dev: {
+          // tell jscs to test the same files as jshint
+          src: ['<%= jshint.dev.src %>', '<%= jshint.mocha.src %>']
+        }
+      }
+    }
+  });
+  //register linting task
+  grunt.registerTask('lint', ['jshint:dev', 'jshint:mocha']);
+};

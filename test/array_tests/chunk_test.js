34fc684791b19f8d73851622d1275fa4ada6552d
Michael Bowen <mikee.bowen@gmail.com>
2015-07-15 15:09:10 -0700

    add test for chunk

--- test/array_tests/chunk_test.js
+++ test/array_tests/chunk_test.js
@@ -2,5 +2,21 @@
 
 var chai = require('chai');
 var expect = chai.expect;
-var chunk = ('../../methods/array/chunk');
+var chunk = require('../../methods/array/chunk');
 
+describe('Chunk Function Test', function (done) {
+  it('should chunk an array', function (done) {
+    var testArray = [1, 2, 3, 4, 5, 6];
+    var testVal = chunk(testArray, 2);
+    expect(testVal).to.eql([[1, 2], [3, 4], [5, 6]]);
+    done();
+  });
+
+  it('should extra numbers in an their own array', function (done) {
+    var testArray2 = [1, 2, 3, 4, 5, 6, 7];
+    var testVal2 = chunk(testArray2, 3);
+    expect(testVal2).to.eql([[1, 2, 3], [4, 5, 6], [7]]);
+    done();
+  })
+
+});

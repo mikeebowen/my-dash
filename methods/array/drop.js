717fbf847207bea31ccf043fa1bebda8901d1b8a
Michael Bowen <mikee.bowen@gmail.com>
2015-07-17 14:45:26 -0700

    add test for drop and add type checking to function

--- methods/array/drop.js
+++ methods/array/drop.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = function (arr, trim) {
-  return arr.slice(trim, arr.length);
+module.exports = function drop (arr, trim) {
+  //if arr is not a number give an error message in the console
+  if (!Array.isArray(arr)) {
+    console.error(arr + ' is not an array');
+  };
+  //if trim is not a number give an error message in the console
+  if (typeof trim !== 'number') {
+    console.error(trim + ' is not a number');
+  };
+  //if arr is an array and trim is a number return a sliced array removing trim number of values
+  if (Array.isArray(arr) && typeof trim === 'number') {
+    return arr.slice(trim, arr.length);
+  };
 }
--- test/all_test.js
+++ test/all_test.js
@@ -4,3 +4,4 @@ require('./array_tests/chunk_test');
 require('./array_tests/compact_test')
 require('./array_tests/difference_test');
 require('./simple_test');
+require('./array_tests/drop_test');

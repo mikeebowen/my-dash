cad2ba6eb9b7a2dfff3b202f86c64dbcbe04e1d3
Michael Bowen <mikee.bowen@gmail.com>
2015-07-28 14:23:56 -0700

    point simplemocha at just all_test.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function (grunt) {
     simplemocha: {
       dev: {
         //tell simple mocha where the test files are
-        src: ['test/**/*.js']
+        src: ['test/all_test.js']
       }
     }
 
